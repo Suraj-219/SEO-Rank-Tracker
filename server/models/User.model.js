2b2df41a7b23d176743b81b009d11c1bbc772d36
Suraj Pal <surajpal7442@gmail.com>
2026-05-24 22:46:55 +0530

    feat: implement authentication middleware and user routes

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -32,6 +32,6 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true})
 
-const Userr = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema)
 
 export default User;
