e496a6a555996d833140c5f924cd4feafb8980fe
Suraj Pal <surajpal7442@gmail.com>
2026-05-30 22:51:48 +0530

    feat: implement rank controller with keyword tracking functions

---
+++ server/controllers/rankController.js
@@ -0,0 +1,29 @@
+// Add a keyword to track
+export const addKeyword = async (req, res) => {
+
+}
+
+// Get all tracked keywords for user
+export const getKeywords = async (req, res) => {
+
+}
+
+// Get single keyword with full history
+export const getKeyword = async (req, res) => {
+
+}
+
+// Manually refresh a keyword ranking 
+export const refreshKeyword = async (req, res) => {
+
+}
+
+// Delete keyword tracking
+export const deleteKeyword = async (req, res) => {
+
+}
+
+// Toggle tracking active/inactive
+export const toggleTracking = async (req, res) => {
+    
+}
