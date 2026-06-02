6afd355b1fb219c45b43f3327d4f0f8ca0ac5539
Suraj Pal <surajpal7442@gmail.com>
2026-06-02 22:52:29 +0530

    feat: implement rankTracker function to extract Google ranking results for a target domain

--- server/services/rankTrackerService.js
+++ server/services/rankTrackerService.js
@@ -4,3 +4,18 @@ import Browserbase from "@browserbasehq/sdk";
 const bb = new Browserbase({
   apiKey: process.env.BROWSERBASE_API_KEY,
 });
+
+// Search Google for a keyword and extract ranking results for a target domain.
+export async function rankTracker(keyword, targetDomain) {
+    let browser;
+    try {
+        // 1. Initialize Browserbase Session & Connect Playwright
+        const session = await bb.sessions.create({browserSettings: {blockAds: true}});
+        browser = await chromium.connectOverCDP(session.connectUrl)
+        const page = browser.contexts()[0].pages()[0];
+        page.setDefaultNavigationTimeout(45000);
+
+    } catch (error) {
+
+    }
+} 
